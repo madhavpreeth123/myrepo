2b3d075d5de77332ce4ec5ca0bf83fe24eb2aaa4
madhavpreeth123 <111100703+madhavpreeth123@users.noreply.github.com>
2022-10-31 14:13:23 +0530

    Delete IMP Programs directory

--- IMP Programs/dict-fruits,veg.py
+++
@@ -1,8 +0,0 @@
-d={'orange':'fruit','potato':'vegetable','banana':'fruit','onion':'vegatable'}
-##{'fruits':[orange,banana],'vegetables':[potato,onion]}
-d1={}
-for i in d:
-    if i not in d1:
-        d1[i]=d[i]
-
-
--- IMP Programs/e3.py
+++
@@ -1,9 +0,0 @@
-d={'madhav':1,'t':2}
-
-try:
-    for i,j in d.items():
-        if d[i]==1:
-            del d[i]
-
-except:
-    print(d)
